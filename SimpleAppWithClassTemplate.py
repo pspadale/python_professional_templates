8b39eba957d07a02f53c4e8c0f64789c7101fefd
pspadale <pravin@dsigndesign.co.in>
2020-04-25 05:51:40 +0530

    Simple app with class template modified for the default menu items, only functions need to be coupled to the respective menu items

--- SimpleAppWithClassTemplate.py
+++ SimpleAppWithClassTemplate.py
@@ -8,6 +8,8 @@ except ImportError:  # Python 2.0 Import
     import Tkinter as tkinter
 
 from tkinter import ttk
+from tkinter import messagebox
+import os
 
 # import yourModuleWithClass.py
 
@@ -37,6 +39,52 @@ class Application(object):
         # sample label widget on the screen.
         self.lbl_SampleLabel = ttk.Label(self.master, text="sample label widget", font=font[11])
         self.lbl_SampleLabel.pack(pady=15)
+
+        def donothing():
+            pass
+
+        def about():
+            txt = ""
+            if os.path.isfile('lic.dat'):
+                with open("lic.dat", 'r') as f:
+                    for line in f:
+                        txt += line
+
+            messagebox.showinfo("Sample application", "Sample application developed for testing by D Sign Design"
+                                                      "\nLicense expiry date is {}".format(txt))
+
+        def deactivate():
+            # self.lic.license_remove()
+            self.master.quit()
+
+        # Menus creations
+        menubar = Menu(self.master)
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label="New", command=donothing)
+        filemenu.add_command(label="Open", command=donothing)
+        filemenu.add_command(label="Save", command=donothing)
+        filemenu.add_command(label="Save as...", command=donothing)
+        filemenu.add_command(label="Close", command=donothing)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=self.master.quit)
+        menubar.add_cascade(label="File", menu=filemenu)
+
+        editmenu = Menu(menubar, tearoff=0)
+        editmenu.add_command(label="Undo", command=donothing)
+        editmenu.add_separator()
+        editmenu.add_command(label="Cut", command=donothing)
+        editmenu.add_command(label="Copy", command=donothing)
+        editmenu.add_command(label="Paste", command=donothing)
+        editmenu.add_command(label="Delete", command=donothing)
+        editmenu.add_command(label="Select All", command=donothing)
+        menubar.add_cascade(label="Edit", menu=editmenu)
+
+        helpmenu = Menu(menubar, tearoff=0)
+        helpmenu.add_command(label="Deactivate License", command=deactivate)
+        helpmenu.add_command(label="About...", command=about)
+        menubar.add_cascade(label="Help", menu=helpmenu)
+
+        self.master.config(menu=menubar)
 
 
 def main():
